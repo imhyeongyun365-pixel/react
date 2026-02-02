84411495179691aafbcce9fa186e33eaafc0d007
unknown <imhyeongyun365@gmail.com>
2026-02-02 11:42:28 +0900

    test

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header'
+import Aside from './components/aside'
+function App() {
+  
+
+  return (
+    <div className='container'>
+      <Header/>
+      <h2>hello react!!!</h2>
+      <Aside/>
+    </div>
+  )
+}
+
+export default App
